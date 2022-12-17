b199c5dd79dbddd838ee077837a44babf47d8028
Muluken Abera <muluken2000abera@gmail.com>
2022-12-17 01:22:42 -0800

    task 07

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
-int print_last_digit(int n);
+int print_last_digit(in);
 void jack_bauer(void);
 void time_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void print_time_table(int n);
 
