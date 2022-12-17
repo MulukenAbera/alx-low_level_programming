4632b2eb6c8834406e709c67500dfc5a43b3eb81
Muluken Abera <muluken2000abera@gmail.com>
2022-12-17 02:14:37 -0800

    all

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by muluken
- * Return: 0
+ * main - print _putchar and a new line
+ * Return: Always 0
  */
 
 int main(void)
@@ -16,5 +16,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
+
 return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,14 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_h
+#define MAIN_h
 
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int n);
-int print_last_digit(in);
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
 void time_table(void);
 int add(int, int);
